9ade1c2ebee28f7b6bc1b94028202bd3253c7805
Takumi Nishida <tnishida1@mail.csuchico.edu>
2018-07-24 17:51:31 -0700

    Added detailed readme instructions, removed echos from script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Create-AWS-Instance-Script
 Quick simple Bash script to create an AWS instance
+
+Run with the following:
+./createaws.bash <ami number> <private key> <instance name>
+
+This script is configured for an AWS instance of OS Centos7 with a security group named "jenkins".
--- createaws.bash
+++ createaws.bash
@@ -5,16 +5,13 @@ if [[ ( $1 != "" && $2 != "" && $3 != "") ]]; then
     KEY="$2"
     NAME="$3"
     INSTANCE="$(aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro --key-name $KEY --security-groups jenkins | grep InstanceId)"
-    echo "${INSTANCE}"
     INSTANCE1="$(echo ${INSTANCE} | cut -d " " -f2 | tr -d '"' | tr -d ',')" 
-    echo ${INSTANCE1}
     aws ec2 create-tags --resources ${INSTANCE1} --tags "Key=Name,Value=${NAME}"
     ID="$(aws ec2 describe-instances --instance-id ${INSTANCE1} | grep PublicDnsName | head -1)"
     ID1="$(echo ${ID} | cut -d " " -f2 | tr -d '"' | tr -d ',')" 
-    echo ${ID1}
     ssh -i ${KEY} -l centos ${ID1}
     #aws ec2 terminate-instances --instance-ids "${INSTANCE1}"
 else
-    echo "AMI and private key name cannot be empty"
+    echo "AMI, private key and instance name cannot be empty"
 fi
 
