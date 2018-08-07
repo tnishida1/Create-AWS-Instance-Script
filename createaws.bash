9fde08cc66e1bae64c67a1b4a2dded85afe3d559
Takumi Nishida <tnishida1@mail.csuchico.edu>
2018-08-07 11:02:00 -0700

    changed

--- createaws.bash
+++ createaws.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ ( $2 != "" && $3 != "") ]]; then
+if [[ ( $1 != "" && $2 != "") ]]; then
     AMI="ami-a042f4d8"
-    KEY="$2"
-    NAME="$3"
+    KEY="$1"
+    NAME="$2"
     INSTANCE="$(aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro --key-name $KEY --security-groups jenkins | grep InstanceId)"
     INSTANCE1="$(echo ${INSTANCE} | cut -d " " -f2 | tr -d '"' | tr -d ',')" 
     aws ec2 create-tags --resources ${INSTANCE1} --tags "Key=Name,Value=${NAME}"
