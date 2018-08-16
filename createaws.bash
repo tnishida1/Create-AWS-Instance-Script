5f77fa72605cda890ae03feaf7d9feb73e7c55da
Takumi Nishida <tnishida1@mail.csuchico.edu>
2018-08-16 12:19:57 -0700

    just create instance, no ssh

--- createaws.bash
+++ createaws.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 if [[ ( $1 != "" && $2 != "") ]]; then
-    AMI="ami-a042f4d8"
+    AMI="ami-00725178"
     KEY="$1"
     NAME="$2"
-    INSTANCE="$(aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro --key-name $KEY --security-groups jenkins | grep InstanceId)"
+    INSTANCE="$(aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.small --key-name $KEY --security-groups jenkins | grep InstanceId)"
     INSTANCE1="$(echo ${INSTANCE} | cut -d " " -f2 | tr -d '"' | tr -d ',')" 
     aws ec2 create-tags --resources ${INSTANCE1} --tags "Key=Name,Value=${NAME}"
     ID="$(aws ec2 describe-instances --instance-id ${INSTANCE1} | grep PublicDnsName | head -1)"
     ID1="$(echo ${ID} | cut -d " " -f2 | tr -d '"' | tr -d ',')" 
-    ssh -i ${KEY} -l centos ${ID1}
+    #ssh-keyscan -H ${ID1} >> ~/.ssh/known_hosts
+    #ssh -i ${KEY} -l centos ${ID1}
     #aws ec2 terminate-instances --instance-ids "${INSTANCE1}"
 else
     echo "private key and instance name cannot be empty"
